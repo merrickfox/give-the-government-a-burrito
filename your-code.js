d683438f49548dc083cae8b340d1ed546509c39f
Merrick Fox <merrick.fox@gmail.com>
2015-07-15 18:55:53 +0100

    basic implementation done

--- your-code.js
+++ your-code.js
@@ -1,11 +1,23 @@
 angular.module('jsCodingTest', [
-    // Add module dependencies here.
+    'cpLib'
 ]);
 
-angular.module('jsCodingTest').controller('GiveTheGovernmentABurrito', function() {
-    // Your JavaScript goes here.
-    // Your code should use our JS library's `PackagesFactory.searchPackages` method to search
-    // for burritos that can be delivered to the Houses of Parliament in London.
-    // The API URL that should be called is:
-    // https://api.citypantry.com/packages/search?name=Burrito&postcode=SW1A%200AA
+angular.module('jsCodingTest').controller('GiveTheGovernmentABurrito', function($scope, PackagesFactory) {
+
+    var onBurritoSuccess,
+        onBurritoFailure;
+
+    $scope.getBurritos = function () {
+        PackagesFactory.searchPackages('Burrito', 'SW1A%200AA')
+            .then(onBurritoSuccess, onBurritoFailure);
+    }
+
+    onBurritoSuccess = function (response) {
+        $scope.results = response.data.packages;
+        $scope.noResults = ($scope.results.length === 0) ? true : false;
+    }
+
+    onBurritoFailure = function (response) {
+        $scope.responseError = true;
+    }
 });
